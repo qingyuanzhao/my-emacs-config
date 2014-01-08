a11aff031eca48292ef4e3f9115d4f16bb99254c
Hera Yu He <hera@dn0a22c350.sunet>
2014-01-08 15:21:48 -0800

    try

--- init.el
+++ init.el
@@ -7,7 +7,9 @@
 
 (when (>= emacs-major-version 24)
   (require 'package)
-  (setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")))
+  (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                           ("marmalade" . "http://marmalade-repo.org/packages/")
+                           ("melpa" . "http://melpa.milkbox.net/packages/")))
   (package-initialize)
   )
 
