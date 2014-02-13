05c92169dfa93644698d8cd80d66980849803caa
Hera Yu He <hera@rescomp-13-272380.stanford.edu>
2014-02-12 18:30:06 -0800

    Use C-<tab> for yasnippet expansion and next field and use tab for cdlatex

--- modules/ssd-yasnippet.el
+++ modules/ssd-yasnippet.el
@@ -13,3 +13,10 @@
 (setq r-autoyas-expand-package-functions-only nil)
 (setq r-autoyas-use-r-based-dot-replacement nil)
 ;; (setq r-autoyas-remove-explicit-assignments nil)
+
+
+(define-key yas-minor-mode-map (kbd "<tab>") nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map (kbd "C-<tab>") 'yas-expand)
+(define-key yas/keymap (kbd "C-<tab>") 'yas-next-field-or-maybe-expand)
+(define-key yas/keymap (kbd "<tab>") 'cdlatex-tab)
